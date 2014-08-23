01f3cf7da54f053dddcf9d4861b638f3199c93c7
Mina Liu <gmliu@comcast.net>
2014-08-23 11:59:07 -0700

    Assignment2
    Updated code for Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Coursera-R Class
+# R Programming Assignment 2
+# by Mina Liu
+# 8/23/14
+#
+# makeCacheMatrix creates special matrix object that can cache its inverse
+# The function takes as an argument x, a matrix object
+# There are 4 functions associated with the cached matrix object:
+#     1) set(x): sets cached matrix as 'x', resets the cached inverse to NULL
+#  	So anytime the cached matrix is updated, the inverse is reset to 
+#		NULL
+#	2) get(): returns the cached matrix
+#	3) setInv(invMatrix): sets the inverse matrix of the cached matrix to 
+#		invMatrix
+#		Note: setInv does not calculate the inverse.  It is passed in.
+#	4) getInv(): returns the cached inverse matrix of the cached matrix
+#		Note: if the inverse has not been set, returns NULL
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(mat) {
+    x <<- mat
+    inv <<- NULL
+  }
+  get<-function() x
+  setInv <- function(invMatrix) inv <<- invMatrix
+  getInv <- function() inv
+  list(set = set, get = get, setInv = setInv, getInv = getInv)
+  
 }
 
 
-## Write a short comment describing this function
+# The function cacheSolve computes the inverse of 'x', a cached matrix 
+# created using makeCacheMatrix.  
+# If the inverse has already been calculated and the matrix has not changed, 
+#     then cacheSolve retrieves the inverse from the cached matrix object.
+# If the inverse has not previously been set using setInv, then 
+#  the inverse is calculated.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInv()
+  if(!is.null(inv)){
+    message("getting cached inverse")
+    return(inv)
+  }
+  cachedMatrix <- x$get()
+  inv <- solve(cachedMatrix)
+  x$setInv(inv)
+  inv
 }
